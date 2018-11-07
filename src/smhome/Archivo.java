609fff7ab4bffbd41706c3eb5e59ed2c451f0547
gago852 <gago.852@outlook.com>
2018-11-07 00:50:51 -0500

    creando usuario y archivo

--- src/smhome/Archivo.java
+++ src/smhome/Archivo.java
@@ -5,10 +5,19 @@
  */
 package smhome;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  *
  * @author gabriel
  */
 public class Archivo {
-    
+    public void ObtenerDatoAlarma(){
+        try {
+            FileReader archivo=new FileReader("alarmas.txt");
+            BufferedReader br= new BufferedReader(archivo);
+        } catch (Exception e) {
+        }
+    }
 }
--- src/smhome/Usuario.java
+++ src/smhome/Usuario.java
@@ -5,10 +5,61 @@
  */
 package smhome;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author gabriel
  */
 public class Usuario {
+    
+    private String nombre;
+    private int id;
+    private String rol;
+    public static ArrayList usuarios=new ArrayList();
+    public static ArrayList habitacionesUsadas=new ArrayList();
+    public static ArrayList habitacionesAdmin=new ArrayList();
+    private Archivo bases;
+
+    public Usuario(String nombre, int id, String rol) {
+        this.nombre = nombre;
+        this.id = id;
+        this.rol = rol;
+        bases=new Archivo();
+    }
+
+    public Usuario() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    public boolean AgregarUsuario(String nombre, int id, String rol){
+        Usuario nuevo=new Usuario(nombre, id, rol);
+        return true;
+    }
+    
+    
     
 }
