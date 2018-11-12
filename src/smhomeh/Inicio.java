a33e479c39f00765df01e1dd22848dea6a8f1ee2
gago852 <gago.852@outlook.com>
2018-11-12 09:26:50 -0500

    que bugaso

--- src/smhomeh/Inicio.java
+++ src/smhomeh/Inicio.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package smhome;
+package smhomeh;
 
 /**
  *
  * @author gabriel
  */
-public class CargarArchivoHabitacion {
+public class Inicio {
     
 }
