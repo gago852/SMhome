7919e29912d4317a70508cbfdd8ad9bc1a58248d
gago852 <gago.852@outlook.com>
2018-11-26 14:42:54 -0500

    implementando interfaz inicio

--- src/smhomeh/Inicio.java
+++ src/smhomeh/Inicio.java
@@ -5,10 +5,68 @@
  */
 package smhomeh;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JTextField;
+
 /**
  *
  * @author gabriel
  */
-public class Inicio {
+public class Inicio extends JFrame implements ActionListener{
+    private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JLabel jLabel3;
     
+    private JButton jButton1;
+    private JButton jButton2;
+    
+    private JMenu jMenu1;
+    private JMenu jMenu2;
+    private JMenu jMenu3;
+    private JMenu jMenu4;
+    private JMenu jMenu5;
+    
+    private JMenuItem jMenuItem1;
+    private JMenuItem jMenuItem2;
+    private JMenuItem jMenuItem3;
+    private JMenuItem jMenuItem4;
+    private JMenuItem jMenuItem5;
+    private JMenuItem jMenuItem6;
+    private JMenuItem jMenuItem7;
+    private JMenuItem jMenuItem8;
+    private JMenuItem jMenuItem9;
+    
+    private JMenuBar jMenuBar1;
+    
+    private JPanel jPanel1;
+    
+    private JRadioButtonMenuItem jRadioButtonMenuItem1;
+    
+    private JTextField jId;    
+    private JTextField jUser;
+
+    public Inicio(){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
